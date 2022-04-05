e14576c336bed1d16b2c8c2ca3cb08333b6cf3ee
brysonPaul <brysonpaul1870@gmail.com>
2022-04-05 17:11:34 -0400

    one class

--- src/poly.java
+++ src/poly.java
@@ -1,45 +1,45 @@
 import java.util.Arrays;
 import java.util.Scanner;
+
 //Bryson Paul   Poly Solution    Professor Arup Guha COP3053C   3/30/22
 public class poly {
-    public static void main(String[] args) {
-        //Initializes a scanner and the hold which will hold the output
-        Scanner sc = new Scanner(System.in);
-        //assigns values to n,t1, and t2 using input
-        int n = (1 << sc.nextInt());
-        long[] t1 = new long[n];
-        long[] t2 = new long[n];
-        //Initializes the two polynomials and adds them to the new arrays
-        for (int x = 0; x < n; x++) {
-            t1[x] = sc.nextLong();
-        }
-        for (int x = 0; x < n; x++) {
-            t2[x] = sc.nextLong();
-        }
-        //Creates the polynomials using the data provided,
-        // and creates the final answer by multiplying the two together
-        polynomial p1 = new polynomial(t1);
-        polynomial p2 = new polynomial(t2);
-        polynomial p3 = p1.mult(p2);
-        //prints output
-        System.out.print(p3.out());
-    }
-}
-
-class polynomial {
     private int length;//length of array coeff
     private long[] coeff;//stores the coefficients of the polynomials
 
     // Creates a polynomial from the coefficients stored in vals.
     // The polynomial created must store exactly (1<<k) coefficients
     // for some integer k.
-    public polynomial(long[] vals) {
+    public poly(long[] vals) {
         this.length = vals.length;
         this.coeff = vals;
     }
+    //main method for the program
+    public static void main(String[] args) {
+        //Initializes a scanner and the hold which will hold the output
+        Scanner sc = new Scanner(System.in);
+        //assigns values to n,t1, and t2 using input
+        int n = (1 << sc.nextInt());
+        long[] t1 = init(n,sc);
+        long[] t2 = init(n,sc);
+        //Creates the polynomials using the data provided,
+        // and creates the final answer by multiplying the two together
+        poly p1 = new poly(t1);
+        poly p2 = new poly(t2);
+        poly p3 = p1.mult(p2);
+        //prints output
+        System.out.print(p3.out());
+    }
+    //Initializes a polyomial coeff array and adds the vals to a new arr
+    private static long[] init(int n,Scanner sc){
+        long[] hold = new long[n];
+        for (int x = 0; x < n; x++) {
+            hold[x] = sc.nextLong();
+        }
+        return hold;
+    }
     //prints out the final polynomial values as a stringbuilder
-    public StringBuilder out() {
-        StringBuilder stringHold = new StringBuilder();
+    private StringBuffer out() {
+        StringBuffer stringHold = new StringBuffer();
         for (long l : this.coeff) {
             stringHold.append(l + "\n");
         }
@@ -48,76 +48,82 @@ class polynomial {
     // Both this and other must be of the same size and the
     // corresponding lengths must be powers of 2.
     // Returns the sum of this and other in a newly created poly.
-    public polynomial add(polynomial other) {
+    public poly add(poly other) {
         long[] vals = new long[this.length];
         for (int x = 0; x < this.length; x++) {
             vals[x] = this.coeff[x] + other.coeff[x];
         }
-        return new polynomial(vals);
+        return new poly(vals);
     }
+
     // Both this and other must be of the same size and the
     // corresponding lengths must be powers of 2.
     // Returns the difference of this and other in a new poly.
-    public polynomial sub(polynomial other) {
+    public poly sub(poly other) {
         long[] vals = new long[this.length];
         for (int x = 0; x < this.length; x++) {
             vals[x] = this.coeff[x] - other.coeff[x];
         }
-        return new polynomial(vals);
+        return new poly(vals);
     }
+
     //adds value directly into the polynomial which called it, and adds the shift to the code to make sure it works right
-    public void addIn(polynomial other, int shift) {
+    public void addIn(poly other, int shift) {
         for (int x = shift; x < other.length + shift; x++) {
             this.coeff[x] += other.coeff[x - shift];
         }
     }
+
     // Both this and other must be of the same size and the
     // corresponding lengths must be powers of 2.
     // Returns the product of this and other in a newly created
     // poly, using the regular nested loop algorithm, with the
     // length being the next power of 2.
-    public polynomial multSlow(polynomial other) {
+    public poly multSlow(poly other) {
         long[] res = new long[this.length + other.length - 1];
         for (int i = 0; i < this.length; i++)
             for (int j = 0; j < other.length; j++)
                 res[i + j] += (this.coeff[i] * other.coeff[j]);
-        return new polynomial(res);
+        return new poly(res);
     }
+
     // Both this and other must be of the same size and the
     // corresponding lengths must be powers of 2.
     // Returns the product of this and other in a newly created
     // poly, using Karatsuba’s algorithm, with the
     // length being the next power of 2.
-    public polynomial mult(polynomial other) {
-        if (this.length < 32) {
+    public poly mult(poly other) {
+        if (this.length <= 32) {
             return multSlow(other);
         }
-        polynomial thisLow = this.getLeft();
-        polynomial otherLow = other.getLeft();
-        polynomial thisHigh = this.getRight();
-        polynomial otherHigh = other.getRight();
+        poly thisLow = this.getLeft();
+        poly otherLow = other.getLeft();
+        poly thisHigh = this.getRight();
+        poly otherHigh = other.getRight();
         // Two recursive cases for "low" half and "high" half of poly.
-        polynomial lowRec = thisLow.mult(otherLow);
-        polynomial highRec = thisHigh.mult(otherHigh);
+        poly lowRec = thisLow.mult(otherLow);
+        poly highRec = thisHigh.mult(otherHigh);
         // This turns out to be the "middle" part, when we foil.
         // Notice how we use one recursive call instead of two.
-        polynomial thisSum = thisHigh.add(thisLow);
-        polynomial otherSum = otherHigh.add(otherLow);
-        polynomial midRec = thisSum.mult(otherSum);
+        poly thisSum = thisHigh.add(thisLow);
+        poly otherSum = otherHigh.add(otherLow);
+        poly midRec = thisSum.mult(otherSum);
         midRec = midRec.sub(lowRec.add(highRec));
         // Put the result back together, shifting each sub-answer as necessary.
-        polynomial res = new polynomial(new long[(this.length << 1) - 1]);
+        poly res = new poly(new long[(this.length << 1) - 1]);
         res.addIn(lowRec, 0);
         res.addIn(highRec, this.length);
         res.addIn(midRec, (this.length / 2));
         return res;
     }
+
     // Returns the left half of this poly in its own poly.
-    private polynomial getLeft() {
-        return new polynomial(Arrays.copyOf(this.coeff, this.length / 2));
+    private poly getLeft() {
+        return new poly(Arrays.copyOf(this.coeff, this.length / 2));
     }
+
     // Returns the right half of this poly in its own poly.
-    private polynomial getRight() {
-        return new polynomial(Arrays.copyOfRange(this.coeff, this.length / 2, this.length));
+    private poly getRight() {
+        return new poly(Arrays.copyOfRange(this.coeff, this.length / 2, this.length));
     }
 }
